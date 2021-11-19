06206286fa983688e712b1b933e46d512768c872
Thomas0456 <thomaslien7479@gmail.com>
2021-11-20 02:27:08 +0700

    Pertemuan 6 - Route Render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  render(){
+    return(
+      <Router>
+        <div>
+          <ul style={{listStyle: 'none'}}>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/news'>News</Link></li>
+          </ul>
 
+          <Switch>
+            <Route path='/' exact render={() => <div> Ini halaman Home</div>} />
+            <Route path='/news' render={() => <div> Ini halaman News</div>} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+  }
+  
 export default App;
